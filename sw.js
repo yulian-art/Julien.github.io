965782b4e232937020af3d093dfab4cafded9056
yulian-art <1119253058@qq.com>
2025-11-04 21:18:25 +0800

    Site updated: 2025-11-04 21:18:25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1762262227858";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1762262302832";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
